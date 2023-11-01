d83bf15de63f411eac084ae4995e2ff8503ffd28
Munna Kumar Barnwal <barnwalmunnakumar@gmail.com>
2023-11-01 23:18:06 +0530

    Added menu bar

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import { Pencil, Eraser, Undo2, Redo2, FileDown } from "lucide-react";
+
+export const Menu = () => {
+  return (
+    <div className="absolute left-1/2 top-5 -translate-x-1/2 bg-white shadow-all border border-[hsl(244, 100%, 97%)] rounded-md flex gap-[25px] px-5 py-1">
+      <button className={clsx("h-9 w-9 rounded-md grid place-items-center cursor-pointer", true && "bg-[#e0dfff] text-[#030064]")} >
+        <Pencil size={16} />
+      </button>
+      <button className="h-9 w-9 grid place-items-center cursor-pointer hover:bg-[#f4f4f4] rounded-md" >
+        <Eraser size={16} />
+      </button>
+      <button className="h-9 w-9 grid place-items-center cursor-pointer hover:bg-[#f4f4f4] rounded-md" >
+        <Undo2 size={16} />
+      </button>
+      <button className="h-9 w-9 grid place-items-center cursor-pointer hover:bg-[#f4f4f4] rounded-md" >
+        <Redo2 size={16} />
+      </button>
+      <button className="h-9 w-9 grid place-items-center cursor-pointer hover:bg-[#f4f4f4] rounded-md" >
+        <FileDown size={16} />
+      </button>
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        all: "rgba(0, 0, 0, 0.24) 0px 3px 8px"
+      }
     },
   },
   plugins: [],
